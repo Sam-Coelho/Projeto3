520d7a53a6a665c77b4694de8a43ffd1f9620d94
Sam-Coelho <samcoelho137@gmail.com>
2025-12-04 14:29:54 +0000

    Update scrooling.js

--- scrooling.js
+++ scrooling.js
@@ -35,49 +35,53 @@ falas[0]=[{
     done: false,
     personagem: "George",
     scrlMoment: 51
-}
-]
+}]
+falas[1]=[{
+    text:"Well...",
+    done: false,
+    personagem: "Lydia",
+    scrlMoment: 51
+}]
 //adiicionar animações
-let animat =[1];
-animat[0] =[];
-    for (let i = 1; i < 42; i++) {
-        let path = `animacoes/cena1/${i}.png`;
-        fetch(path)
-          .then(r => {
-            if (r.ok) 
-                animat[1].push({
-                    src: path,   
-                    mom: i 
-                  });
-          });
-    }
+let animat = [1];
+animat[0]=[]
+animat[1]=[{
+    scr: "portaabrindo.mp4",
+    scrllStart: 1,
+    scrllTotal: 500
+}
+
+]
 
 
 
 let cena=0;
 let maxScroll= falas[cena][falas[cena].length-1].scrlMoment * 10;
 
+
 window.addEventListener("scroll", function () {
     const text = document.getElementById("text");
     const anim= document.getElementById("animat");
     console.log(window.scrollY);
-
     if(window.scrollY>maxScroll && falas[cena][falas[cena].length-1].done === false){
         const butt = document.createElement("a");
         text.appendChild(butt);
         butt.textContent="Proxima cena";
         butt.classList.add("butao");
         cena=cena+1;
+        console.log(cena);
         butt.href=`cena${cena}.html`;
         falas[cena][falas[cena].length-1].done=true;
-
     }
     if (animat[cena]) {
          for(let i = 0; i < animat[cena].length; i++){
-            let scrolP = falas[cena][i].mom * 10;
-                if(window.scrollY > scrolP){
-                    anim.src= animat[cena][i].src;
-                    console.log(animat[cena][i].src);
+            if(window.scrollY>animat[cena][i].scrllStart && window.scrollY<animat[cena][i].scrllStart+animat[cena][i].scrllTotal){
+                anim.scr= animat[cena][i].scr;
+                const scrollFraction = (window.scrollY-animat[cena][i].scrllStart) / animat[cena][i].scrllTotal;
+                const videoDuration = anim.duration;
+                anim.currentTime = scrollFraction * videoDuration;
+            }else{
+                anim.scr= "";
             }
         }
     }
@@ -85,9 +89,9 @@ window.addEventListener("scroll", function () {
         let scrolP = 20 + falas[cena][i].scrlMoment * 10;
         if(window.scrollY > scrolP && falas[cena][i].done===false){
             const fala1 = document.createElement("p");
-            text.appendChild(fala1);
             fala1.textContent = falas[cena][i].text;
             fala1.classList.add(falas[cena][i].personagem);
+            text.appendChild(fala1);
             falas[cena][i].done=true;
         }
     }
