88897c86672599aa21130e61016f96699bba1f62
Sam-Coelho <samcoelho137@gmail.com>
2025-12-18 14:15:40 +0000

    Add files via upload

---
+++ scrooling.js
@@ -0,0 +1,610 @@
+//adicionar falas
+let falas = [2];
+falas[0]=[{
+    text:"I wish you’d look at the nursery.",
+    done: false,
+    personagem: "Lydia",
+    scrlMoment: 5
+},
+{
+    text:"Whats wrong with it?",
+    done: false,
+    personagem: "George",
+    scrlMoment: 20
+},
+{
+    text:"I don't know... Its just",
+    done: false,
+    personagem: "Lydia",
+    scrlMoment: 30
+},
+{
+    text:"Different then what it was",
+    done: false,
+    personagem: "Lydia",
+    scrlMoment: 40
+},
+{
+    text:"Alright, lets have a look",
+    done: false,
+    personagem: "George",
+    scrlMoment: 50
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 52
+}]
+falas[1]=[{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 10
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 20
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 25
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 30
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 35
+},
+{
+    text:"Well...",
+    done: false,
+    personagem: "George",
+    scrlMoment: 40
+},
+{
+    text:"Nothing too good for our childreen",
+    done: false,
+    personagem: "George",
+    scrlMoment: 50
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 60
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 60
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 60
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 60
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 60
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 60
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 60
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 60
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 65
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 65
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 65
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 65
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 65
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 65
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 65
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 65
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 65
+},
+{
+    text:"Let`s get out of this sun,",
+    done: false,
+    personagem: "George",
+    scrlMoment: 200
+},
+{
+    text:"This is a little too real. But I don`t see anything wrong.",
+    done: false,
+    personagem: "George",
+    scrlMoment: 210
+},
+{
+    text:"Wait a moment, you`ll see",
+    done: false,
+    personagem: "Lydia",
+    scrlMoment: 220
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 230
+},
+{
+    text:"WHATCH OUT!!",
+    done: false,
+    personagem: "Lydia",
+    scrlMoment: 395
+},
+{
+    text:".",
+    done: false,
+    personagem: "space",
+    scrlMoment: 400
+}
+,]
+//adiicionar animações
+let animat = [2];
+animat[0]=[{
+    src: "branco.mp4",
+    scrllStart: 0,
+    scrllTotal: 1100,
+    dura: 10
+}]
+animat[1]=[{
+    src: "portaabrindo.mp4",
+    scrllStart: 0,
+    scrllTotal: 350,
+    dura: 5
+},
+{
+    src: "branco.mp4",
+    scrllStart: 380,
+    scrllTotal: 1100,
+    dura: 10
+},
+{
+    src: "transicao.mp4",
+    scrllStart: 1100,
+    scrllTotal: 1600,
+    dura: 7
+},
+{
+    src: "tween1.mp4",
+    scrllStart: 1600,
+    scrllTotal: 2000,
+    dura: 1
+},
+{
+    src: "branco.mp4",
+    scrllStart: 2000,
+    scrllTotal: 2800,
+    dura: 10
+},
+{
+    src: "fundo.mp4",
+    scrllStart: 2800,
+    scrllTotal: 3500,
+    dura: 4
+},
+{
+    src: "branco.mp4",
+    scrllStart: 3500,
+    scrllTotal: 4000,
+    dura: 10
+}
+]
+
+let imagesParal1 = [2];
+imagesParal1[0]=[{
+    scr: "noImage.png",
+    InTime: 1,
+    done: false
+}]
+imagesParal1[1]=[{
+    scr: "noImage.png",
+    InTime: 1,
+    done: false
+}]
+let imagesParal2 = [2];
+imagesParal2[0]=[{
+    scr: "noImage.png",
+    InTime: 30,
+    done:false
+}]
+imagesParal2[1]=[{
+    scr: "noImage.png",
+    InTime: 0,
+    done:false
+},
+{
+    scr: "rock1.png",
+    InTime: 290,
+    done: false
+}]
+let imagesParal3 = [2];
+imagesParal3[0]=[{
+    scr: "noImage.png",
+    InTime: 1,
+    done:false
+}]
+imagesParal3[1]=[{
+    scr: "camada3.png",
+    InTime: 290,
+    done: false
+}]
+
+let abtCenas = [1200,4800];
+
+function parallaxFrom(element, startScroll, speed) {
+    const scrollY = window.scrollY;
+    console.log("funçao paralax usada");
+    console.log(element);
+    console.log("style:"+element.style);
+   if (scrollY < startScroll) {
+      element.style.top = "-200px";
+      return;
+    }
+    const relativeScroll = scrollY - startScroll;
+    element.style.top = -200 + (relativeScroll * speed) + "px";
+    if(relativeScroll>800){
+        if (element.classList.contains("visible")) {
+        element.classList.remove("visible");
+        }
+    }
+  }
+
+
+let cena=0;
+let maxScroll= falas[cena][falas[cena].length-1].scrlMoment * 10;
+let ShowAnim;
+let ScrAnim;
+let VideoMoment=0.001;
+let anim= document.getElementById("animat");
+let lastscr = null;
+anim.pause();
+
+window.addEventListener("scroll", function () {
+    let text = document.getElementById("text"); 
+    //console.log(window.scrollY);
+    //video
+        for(let i = 0; i < animat[cena].length; i++){
+            if(window.scrollY>animat[cena][i].scrllStart && window.scrollY<animat[cena][i].scrllStart+animat[cena][i].scrllTotal){
+                console.log("cena:", cena);
+                ScrAnim = animat[cena][i].src;
+                console.log(animat[cena][i].src)
+                let scrollFraction = (window.scrollY-animat[cena][i].scrllStart) / animat[cena][i].scrllTotal;
+                //console.log("frac" + scrollFraction);
+                VideoMoment = scrollFraction * animat[cena][i].dura;
+                if(anim.src !== ScrAnim && ScrAnim !== anim.scr && lastscr !== ScrAnim && ScrAnim !== lastscr){
+                anim.src = ScrAnim;
+                lastscr = ScrAnim;
+                console.log("scrAtualizado");
+                }
+                anim.currentTime = VideoMoment;
+            }
+        }
+        //butao
+    if(window.scrollY>maxScroll && falas[cena][falas[cena].length-1].done === false){
+        let butt = document.createElement("p");
+        let abutt= document.getElementById("butao");
+        butt.textContent="Proxima cena";
+        butt.id="butao";
+        abutt.style.display="block";
+        falas[cena][falas[cena].length-1].done=true;
+        abutt.appendChild(butt);
+        console.log("button added");
+    }
+    //texto
+    for(let i = 0; i<falas[cena].length; i++){
+        let scrolP = 20 + falas[cena][i].scrlMoment * 10;
+        if(window.scrollY > scrolP && falas[cena][i].done===false){
+            let fala1 = document.createElement("p");
+            fala1.textContent = falas[cena][i].text;
+            fala1.classList.add(falas[cena][i].personagem);
+            text.appendChild(fala1);
+            setTimeout(() => {
+                fala1.classList.add("visible");
+            }, 50);
+            falas[cena][i].done=true;
+            console.log("fala"+i + "cena" + cena);
+        }
+    }
+    //paralax
+    let pl1 = document.getElementById("Paralax1");
+    let pl2 = document.getElementById("Paralax2");
+    let pl3 = document.getElementById("Paralax3");
+    //add images
+    for(let i = 0; i<imagesParal1[cena].length; i++){
+        let scrolP = 20 + imagesParal1[cena][i].InTime * 10;
+        if(window.scrollY > scrolP && imagesParal1[cena][i].done===false){
+            let ImgParal = document.createElement("img");
+            ImgParal.src = imagesParal2[cena][i].scr;
+            ImgParal.dataset.startScroll = window.scrollY;
+
+            ImgParal.classList.add("paral");
+            pl1.appendChild(ImgParal);
+            setTimeout(() => {
+                ImgParal.classList.add("visible");
+            }, 50);
+            imagesParal1[cena][i].done=true;
+            imagesParal1[cena][i].element = ImgParal;
+        }
+    }
+    for(let i = 0; i<imagesParal2[cena].length; i++){
+        let scrolP = 20 + imagesParal2[cena][i].InTime * 10;
+        if(window.scrollY > scrolP && imagesParal2[cena][i].done===false){
+            let ImgParal = document.createElement("img");
+            ImgParal.src = imagesParal2[cena][i].scr;
+            ImgParal.dataset.startScroll = window.scrollY;
+
+            ImgParal.classList.add("paral");
+            pl2.appendChild(ImgParal);
+            setTimeout(() => {
+                ImgParal.classList.add("visible");
+            }, 50);
+            imagesParal2[cena][i].done=true;
+            imagesParal2[cena][i].element = ImgParal;
+        }
+    }
+    
+    for(let i = 0; i<imagesParal3[cena].length; i++){
+        let scrolP = 20 + imagesParal3[cena][i].InTime * 10;
+        if(window.scrollY > scrolP && imagesParal3[cena][i].done===false){
+            let ImgParal = document.createElement("img");
+            ImgParal.src = imagesParal3[cena][i].scr;
+            ImgParal.dataset.startScroll = window.scrollY;
+
+            ImgParal.classList.add("paral");
+            pl3.appendChild(ImgParal);
+            setTimeout(() => {
+                ImgParal.classList.add("visible");
+            }, 50);
+            imagesParal3[cena][i].done=true;
+            imagesParal3[cena][i].element = ImgParal;
+        }
+    }
+    
+//move iamges
+    imagesParal1[cena].forEach(item => {
+        console.log ("element finded");
+        if (item.done){
+        console.log("tentar chamar funcao");
+        const start = Number(item.element.dataset.startScroll);
+        console.log("start:"+start);
+        parallaxFrom(item.element, start, 0.1) + "px";
+        }
+      });
+      imagesParal2[cena].forEach(item => {
+        console.log ("element finded");
+        if (item.done){
+        console.log("tentar chamar funcao");
+        const start = Number(item.element.dataset.startScroll);
+        console.log("start:"+start);
+        parallaxFrom(item.element, start, 0.15) + "px";
+        }
+      });
+      imagesParal3[cena].forEach(item => {
+        console.log ("element finded");
+        if (item.done){
+        console.log("tentar chamar funcao");
+        const start = Number(item.element.dataset.startScroll);
+        console.log("start:"+start);
+        parallaxFrom(item.element, start, 0.2) + "px";
+        }
+      });
+
+
+  });
+
+  let AddScroolToText = document.getElementById("toscroll");
+  let but = document.getElementById("butao");
+    but.addEventListener('click', function(e) {
+        e.preventDefault(); // evita ir direto para a página
+        console.log("Botão clicado!");
+        cena=cena+1;
+        console.log("cena"+cena);
+        let tex= document.getElementById("text");
+        maxScroll= falas[cena][falas[cena].length-1].scrlMoment * 10;
+        tex.innerHTML="<p></p>";
+        window.scrollTo(0, 0);
+        but.style.display="none";
+        console.log("scrlmax " + abtCenas[cena]);
+        AddScroolToText.style.top = abtCenas[cena] + "px";
+        console.log("scrlmax.Confirm " +  AddScroolToText.style.top);
+        for(let i=0; i<falas[cena].length; i++){
+            falas[cena][i].done=false;
+        }
+    });
+
+
+
+   
